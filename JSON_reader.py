e2fd36b55e763e4c87868e5e156c1e4ba9a9306e
Markief <mknode49@gmail.com>
2024-11-20 19:36:46 -0500

    Added ideas for functionality to the readme

--- JSON_reader.py
+++ JSON_reader.py
@@ -3,7 +3,7 @@ import json
 #input file name
 recipe_type = 'baking.json'
 
-# Reads the JSON file, inputs it into a dictionary, and close the file
+# Reads the JSON file, inputs it into a dictionary, and closes the file
 with open( 'JSON_Files/' + recipe_type, 'r') as file:
     data = json.load(file)
 
---
+++ README.md
@@ -0,0 +1,6 @@
+### Ideas
+-Print final recipe to a file
+-Input filename from the list
+-Print only the recipe names when sorting by parameter in the terminal
+-Allow to expand the recipe in terminal, but not the actual instructions since they are so long
+-
