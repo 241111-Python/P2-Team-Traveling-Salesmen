e58470623f9b7cf29bd6b62563139ed5b0d4bb7b
Markief <mknode49@gmail.com>
2024-11-21 15:07:00 -0500

    add some funct

--- JSON_reader.py
+++ JSON_reader.py
@@ -1,10 +1,32 @@
 import json
 
+class Recipe:
+    def __init__(self, id, url, image, name):
+        self.id = id
+        self.url = url
+        self.image = image
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name}:'
+
+
 #input file name
 recipe_type = 'baking.json'
 
-# Reads the JSON file, inputs it into a dictionary, and closes the file
+# Reads the JSON file, inputs it into a list of dictionary, and closes the file
 with open( 'JSON_Files/' + recipe_type, 'r') as file:
     data = json.load(file)
 
-print(data[0])
+#print(data[0]) 
+#for i in data:
+#    print(i)
+
+recipeobjs = []
+for x in data:
+    entry = Recipe(x['id'], x['url'], x['image'], x['name'])
+    recipeobjs.append(entry)
+    print(entry)
+    
+#for i in recipeobjs:
+#   print(i)
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Ideas
 
 -Print final recipe to a file (append or overwrite?)
-
--Input filename from the list
-
--Print only the recipe names when sorting by parameter in the terminal
-
--Allow to expand the recipe in terminal, but not the actual instructions since they are so long
--
+ 
+ -Input filename from the list
+ 
+ -Print only the recipe names when sorting by parameter in the terminal
+ 
+ -Allow to expand the recipe in terminal, but not the actual instructions since they are so long
+ 
