50a880d8a470f3c786aaca4bc4e934c0d9599a5a
Markief <mknode49@gmail.com>
2024-11-22 13:07:15 -0500

    added nutrient analysis

--- recipe.py
+++ recipe.py
@@ -1,23 +1,16 @@
 class Recipe():
 
-    def __init__(self, id, url, image, name, description, author, rattings, ingredients, steps, nutrients, times, serves, difficult, vote_count, subcategory, dish_type, maincategory ):
-        self.id = id
+    def __init__(self, url, name, description, ingredients, steps, nutrients, times, serves, dish_type, maincategory ):
         self.url = url
-        self.image = image 
         self.name = name
         self.description = description 
-        self.author = author
-        self.rattings = rattings
         self.ingredients = ingredients
         self.steps = steps 
         self.nutrients = nutrients
         self.times = times
         self.serves = serves
-        self.difficult = difficult
-        self.vote_count = vote_count
-        self.subcategory = subcategory
         self.dish_type = dish_type
         self.maincategory = maincategory
 
-    def __str__():
-        return f'?'
+    def __str__(self):
+        return f'{self.name}: {self.url}'
