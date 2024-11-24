6cb583abd509cc04dd2e052f4f87752e0521180f
Pavan7842 <pavan9900@outlook.com>
2024-11-24 15:44:57 -0500

    Added SearchByIngredients function, made few changes in main.py file-regarding SerachByIngredients function(like if it founds recipes with provide those, if not will return No recipes found)

--- JSON_reader.py
+++ JSON_reader.py
@@ -7,6 +7,7 @@ class Recipe:
 
     def __str__(self):
         return f'{self.name}: {self.url}'
+    
 
 def searchByName(name, recipeFile, minCalories=0, maxCalories=100000, prepTime=1000):
 
@@ -25,7 +26,41 @@ def searchByName(name, recipeFile, minCalories=0, maxCalories=100000, prepTime=1
                 recipes.append(Recipe(recipe['name'], recipe['url']))
 
     return recipes
-    
+     
 
-def searchByIngredients(ingredients, recipeFile, minCalories=0, maxCalories=100000, prepTime=1000): 
-    pass
+def searchByIngredients(ingredients, recipeFile, minCalories=0, maxCalories=100000, prepTime=1000):
+
+    with open('JSON_Files/' + recipeFile + '.json', 'r') as file:
+        data = json.load(file)
+
+    recipes = []
+
+    #input ingredients to make them case-insensitive and strip extra spaces
+    ingredients = [ingredient.lower().strip() for ingredient in ingredients.split(",")]
+
+    for recipe in data:
+        try:
+            # Check if the recipe's ingredients are in a list format
+            if isinstance(recipe['ingredients'], list):
+                # Process each ingredient in the recipe, making it case-insensitive and stripping extra spaces
+                recipe_ingredients = [ingredient.strip().lower() for ingredient in recipe['ingredients']]
+            else:
+                # If ingredients are not in a list format, use an empty list
+                recipe_ingredients = []
+
+            # Check if any of the user's ingredients match the recipe ingredients
+            matching_ingredients = [ingredient for ingredient in ingredients if any(ingredient in item for item in recipe_ingredients)]
+
+            # If there are matching ingredients, proceed with further checks
+            if matching_ingredients:
+                calories = int(recipe['nutrients']['kcal']) / recipe['serves'] if 'kcal' in recipe['nutrients'] else 0
+
+                prep = recipe['times'].get('prep', 0)
+
+                if minCalories <= calories <= maxCalories and prepTime >= int(prep):
+                    recipes.append(Recipe(recipe['name'], recipe['url']))
+
+        except KeyError as e:
+            print(f"KeyError: {e} - skipping recipe {recipe['name']}")
+
+    return recipes
