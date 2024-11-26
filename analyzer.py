2427e07edf24d75de64530ca253f9e1bf4f491ef
Markief <mknode49@gmail.com>
2024-11-26 13:32:58 -0500

    analyzer updates

--- analyzer.py
+++ analyzer.py
@@ -1,20 +1,29 @@
 from recipe import Recipe
 import json
 from output_format import single_output_format
+import datetime
 
 recipes = []
 
-with open( '.json', 'r') as file:
-    recipe = json.load(file)
+with open( 'Json_Files/baking.json', 'r') as file:
+    recipe_data = json.load(file)
 
-for i in recipe:
-    recipes.append(Recipe(recipe['name'], recipe['url'], recipe['description'], recipe['ingredients'], recipe['steps'], recipe['nutrients'], recipe['times'], recipe['serves'], recipe['dish_type'], recipe['maincategory']))
+index = 0
+for i in recipe_data:
+    recipes.append(Recipe(recipe_data[index]['name'], recipe_data[index]['url'], recipe_data[index]['description'], recipe_data[index]['ingredients'], recipe_data[index]['steps'], 
+                          recipe_data[index]['nutrients'], recipe_data[index]['times'], recipe_data[index]['serves'], recipe_data[index]['dish_type'], recipe_data[index]['maincategory']))
+    index += 1 
 
+# Used to output the nutrition facts per serving for all recipes you add to a the .json file 
 if recipes:
     
-    with open("output.txt", 'a') as file:
-      
+    with open("recipe_analysis.txt", 'w') as file:
+        file.write(f'{datetime.date} {datetime.time}')
+        file.write('Selected Recipe Nutrition Facts Per Serving:')
         for recipe in recipes:
-  
-            file.write(single_output_format(recipe))
+            if not recipe.nutrients:
+                file.write(f'''Recipe Name: {recipe.name}: {recipe.url}
+                           Nutrition Facts Unavailable!''')
+            else:
+                file.write(single_output_format(recipe))
 
--- nutrients_analysis.py
+++ nutrients_analysis.py
@@ -2,9 +2,9 @@ from recipe import Recipe
 import json
 import re
 
-# recipeFile = 'baking'
-# with open( 'JSON_Files/' + recipeFile + '.json', 'r') as file:
-#     data = json.load(file)
+recipeFile = 'baking'
+with open( 'JSON_Files/' + recipeFile + '.json', 'r') as file:
+    data = json.load(file)
 
 def kcal_dv(recipe):
     daily_value = 2000
@@ -77,6 +77,8 @@ def minutes_to_hours(minutes):
         return f'{minutes} minutes'
 
 
-# new_recipe = Recipe(data[3]['name'], data[3]['url'], data[3]['description'], data[3]['ingredients'], data[3]['steps'], data[3]['nutrients'], data[3]['times'], data[3]['serves'], data[3]['dish_type'], data[3]['maincategory'])
+new_recipe = Recipe(data[5]['name'], data[5]['url'], data[5]['description'], data[5]['ingredients'], data[5]['steps'], data[5]['nutrients'], data[5]['times'], data[5]['serves'], data[5]['dish_type'], data[5]['maincategory'])
+print(new_recipe.name)
+print(total_time(new_recipe))
 
 
